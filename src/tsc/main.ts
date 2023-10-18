703680e293bf09f11bb41d6d3f37d5ed037003bb
Tobias <tobias.maier@st.oth-regensburg.de>
2023-10-18 22:27:41 +0200

    add button for marker on map, add formater in vscode

--- src/tsc/main.ts
+++ src/tsc/main.ts
@@ -1,56 +1,100 @@
+declare var google: any;
+let map: google.maps.Map | null = null;
 const apiKey = import.meta.env.VITE_GMAP_API_KEY;
 
 document.addEventListener("DOMContentLoaded", function () {
-    const liveLink = document.getElementById("liveLink");
-    const planLink = document.getElementById("planLink");
-    const loginLink = document.getElementById("loginLink");
-    const pageTitle = document.getElementById("pageTitle");
-    const contentDiv = document.getElementById("content");
+  // if (window.location.pathname !== "/about") {
+  //   window.history.replaceState({}, "", "/about#live" + window.location.hash);
+  // }
 
-    if (liveLink && planLink && loginLink && pageTitle && contentDiv) {
-        liveLink.addEventListener("click", function () {
-        loadPage("Live", pageTitle, contentDiv);
-        });
+  const liveLink = document.getElementById("liveLink");
+  const planLink = document.getElementById("planLink");
+  const loginLink = document.getElementById("loginLink");
+  const pageTitle = document.getElementById("pageTitle");
+  const contentDiv = document.getElementById("content");
+  const setPumpButton = document.getElementById("setPumpButton");
 
-        planLink.addEventListener("click", function () {
-        loadPage("Plan", pageTitle, contentDiv);
-        });
+  if (
+    liveLink &&
+    planLink &&
+    loginLink &&
+    pageTitle &&
+    contentDiv
+    //  &&
+    // setPumpButton
+  ) {
+    liveLink.addEventListener("click", function () {
+      loadPage("Live", pageTitle, contentDiv);
+    });
 
-        loginLink.addEventListener("click", function () {
-        loadPage("Login", pageTitle, contentDiv);
-        });
-    }
-});
-function loadPage(pageName: string, pageTitle: HTMLElement, contentDiv: HTMLElement) {
-    // Update the page title
-    pageTitle.textContent = pageName;
-  
-    // Clear previous content
-    contentDiv.innerHTML = "";
-  
-    if (pageName === "Live" || pageName === "Plan") {
-      // Load the Google Maps API script only for Live and Plan pages
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-      document.body.appendChild(script);
-  
-      // Create a div for the map
-      const mapDiv = document.createElement("div");
-      mapDiv.id = "map";
-      contentDiv.appendChild(mapDiv);
-  
-      // Function to initialize the Google Map
-      window.initMap = function () {
-        const options = {
-          zoom: 11,
-          center: { lat: 48.411328, lng: 12.947491 } // New York coordinates
-        };
-        const map = new google.maps.Map(document.getElementById("map")!, options);
-      };
-    }
-    else if (pageName === "Login") {
-      // Show the login form or whatever you want for the Login page
-      contentDiv.innerHTML = "<p>Login form goes here.</p>";
-    }
+    planLink.addEventListener("click", function () {
+      loadPage("Plan", pageTitle, contentDiv);
+    });
+
+    loginLink.addEventListener("click", function () {
+      loadPage("Login", pageTitle, contentDiv);
+    });
+    setPumpButton.addEventListener("click", function () {
+      setMarker(48.411328, 12.947491);
+    });
   }
+});
+
+function loadPage(
+  newPageTitle: string,
+  oldPageTitle: HTMLElement,
+  contentDiv: HTMLElement
+) {
+  oldPageTitle.textContent = newPageTitle;
+  contentDiv.innerHTML = "";
+
+  if (newPageTitle === "Live" || newPageTitle === "Plan") {
+    // Load the Google Maps API script only for Live and Plan pages
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+    document.body.appendChild(script);
+
+    // Create a div for the map
+    const mapDiv = document.createElement("div");
+    mapDiv.id = "map";
+    contentDiv.appendChild(mapDiv);
+
+    // Function to initialize the Google Map
+    window.initMap = function () {
+      const options = {
+        zoom: 11,
+        center: { lat: 48.411328, lng: 12.947491 }, // New York coordinates
+      };
+      map = new google.maps.Map(document.getElementById("map")!, options);
+    };
+  } else if (newPageTitle === "Login") {
+    contentDiv.innerHTML = "<p>Login form goes here.</p>";
+  }
+  if (newPageTitle === "Live") {
+    const setPumpButton = document.createElement("button");
+    setPumpButton.id = "setPumpButton";
+    setPumpButton.textContent = "Pumpe setzen";
+    setPumpButton.style.width = "100px";
+    setPumpButton.style.height = "50px";
+    contentDiv.appendChild(setPumpButton);
+    setPumpButton.addEventListener("click", function () {
+      setMarker(48.411328, 12.947491); // Add a marker at this location
+    });
+  }
+}
+
+function setMarker(lat: number, lng: number) {
+  if (map) {
+    // Check if map is initialized
+    const pos = {
+      lat: lat,
+      lng: lng,
+    };
+    new google.maps.Marker({
+      position: pos,
+      map: map,
+    });
+    map.setCenter(pos);
+  }
+}
