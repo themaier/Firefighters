f2338d7dff84d6eebec1017c1c1f974b3d26a670
Tobias <tobias.maier@st.oth-regensburg.de>
2023-10-19 21:36:17 +0200

    add marker delete button

--- src/tsc/main.ts
+++ src/tsc/main.ts
@@ -1,6 +1,8 @@
+let markers: google.maps.Marker = [];
 declare var google: any;
 let map: google.maps.Map | null = null;
 const apiKey = (import.meta as any).env.VITE_GMAP_API_KEY;
+let counter: number = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
   // if (window.location.pathname !== "/about") {
@@ -24,39 +26,13 @@ document.addEventListener("DOMContentLoaded", function () {
   // Load Live Page
   liveLink.addEventListener("click", () => {
     hideAllPages();
-    livePage.style.display = "block";
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-    document.body.appendChild(script);
-    (window as any).initMap = function () {
-      const options = {
-        zoom: 11,
-        center: { lat: 48.411328, lng: 12.947491 }, // New York coordinates
-      };
-      map = new google.maps.Map(livePage.querySelector("#map")!, options);
-    };
-  });
-  const setPumpButton = document.getElementById("setPumpButton") as HTMLElement;
-  setPumpButton.addEventListener("click", function () {
-    setMarker(48.411328, 12.947491);
+    handleLivePage(livePage);
   });
 
   // Load Plan Page
   planLink.addEventListener("click", () => {
     hideAllPages();
-    planPage.style.display = "block";
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
-    document.body.appendChild(script);
-    (window as any).initMap = function () {
-      const options = {
-        zoom: 11,
-        center: { lat: 48.411328, lng: 12.947491 }, // New York coordinates
-      };
-      map = new google.maps.Map(planPage.querySelector("#map")!, options);
-    };
+    handlePlanPage(planPage);
   });
 
   // Load Login Page
@@ -66,16 +42,70 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function setMarker(lat: number, lng: number) {
-  if (map) {
-    const pos = {
-      lat: lat,
-      lng: lng,
+function handleLivePage(livePage: HTMLElement) {
+  livePage.style.display = "block";
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+  document.body.appendChild(script);
+  (window as any).initMap = function () {
+    const options = {
+      zoom: 11,
+      center: { lat: 48.411328, lng: 12.947491 }, // New York coordinates
     };
-    new google.maps.Marker({
-      position: pos,
-      map: map,
-    });
-    map.setCenter(pos);
-  }
+    map = new google.maps.Map(livePage.querySelector("#map")!, options);
+  };
+
+  const setPumpButton = document.getElementById("setPumpButton") as HTMLElement;
+  setPumpButton.addEventListener("click", function () {
+    setMarker(48.411328, 12.947491 + counter);
+    counter += 0.1;
+  });
+
+  const deletePumpButton = document.getElementById(
+    "deletePumpButton"
+  ) as HTMLElement;
+  deletePumpButton.addEventListener("click", function () {
+    if (markers.length > 0) {
+      const lastMarker = markers[markers.length - 1];
+      lastMarker.setMap(null);
+      markers.pop();
+      map.setCenter(markers[markers.length - 1].getPosition());
+    }
+  });
+
+  const getElevationButton = document.getElementById(
+    "getElevationButton"
+  ) as HTMLElement;
+  getElevationButton.addEventListener("click", function () {
+    setMarker(48.411328, 12.947491);
+  });
+}
+
+function handlePlanPage(planPage: HTMLElement) {
+  planPage.style.display = "block";
+  const script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+  document.body.appendChild(script);
+  (window as any).initMap = function () {
+    const options = {
+      zoom: 11,
+      center: { lat: 48.411328, lng: 12.947491 }, // New York coordinates
+    };
+    map = new google.maps.Map(planPage.querySelector("#map")!, options);
+  };
+}
+
+function setMarker(lat: number, lng: number) {
+  const pos = {
+    lat: lat,
+    lng: lng,
+  };
+  const marker = new google.maps.Marker({
+    position: pos,
+    map: map,
+  });
+  markers.push(marker);
+  map.setCenter(pos);
 }
