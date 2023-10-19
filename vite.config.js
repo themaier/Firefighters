da3e0aaedd68e23f245682cdae6db10c93804847
Tobias <tobias.maier@st.oth-regensburg.de>
2023-10-19 18:42:53 +0200

    add minification

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,20 @@
 import { defineConfig } from 'vite';
 import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 const isProduction = process.env.BUILD_ENV === 'production';
 
 export default defineConfig({
+  // base: '/Firefighters/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     isProduction ? obfuscatorPlugin({
       options: {
         debugProtection: true,
       },
-    }) : null
-  ],
-  build: {
-    outDir: 'dist',
-  }
+    }) : null,
+    isProduction ? ViteMinifyPlugin({}) : null
+  ]
 });
-
-// vite.config.js
-// export default {
-//   // base: '/Firefighters/',
-//   build: {
-//     outDir: 'dist',
-//   }
-// }
