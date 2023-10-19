71206df40eaa699f26aa5bf97d0684586410ee9b
Tobias <tobias.maier@st.oth-regensburg.de>
2023-10-19 18:37:19 +0200

    add obfuscation for production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
+import { defineConfig } from 'vite';
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
+
+const isProduction = process.env.BUILD_ENV === 'production';
+
+export default defineConfig({
+  plugins: [
+    isProduction ? obfuscatorPlugin({
+      options: {
+        debugProtection: true,
+      },
+    }) : null
+  ],
+  build: {
+    outDir: 'dist',
+  }
+});
+
 // vite.config.js
-export default {
-    base: '/Firefighters/',
-    build: {
-      outDir: 'dist',
-    }
-}
+// export default {
+//   // base: '/Firefighters/',
+//   build: {
+//     outDir: 'dist',
+//   }
+// }
