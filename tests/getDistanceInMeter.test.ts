77ef51b9245f442f1d5f179716da1ebeabc58d54
Tobias <tobias.maier@st.oth-regensburg.de>
2023-10-29 23:08:19 +0100

    add distance messurement, add test

---
+++ tests/getDistanceInMeter.test.ts
@@ -0,0 +1,9 @@
+const getDistanceInMeter = require("../src/ts/main");
+
+test("takes two coordinates and meassures distance", () => {
+  let lat1 = 48.411328;
+  let lng1 = 12.947491;
+  let lat2 = 48.406997;
+  let lng2 = 12.94749;
+  expect(getDistanceInMeter(lat1, lng1, lat2, lng2)).toBe("481.6");
+});
